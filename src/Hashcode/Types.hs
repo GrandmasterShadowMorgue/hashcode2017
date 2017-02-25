ea46f4215aa5b145cae94a4793d5e0d550d4eae3
GrandmasterShadowMorgue <jayant.shivarajan@gmail.com>
2017-02-25 20:43:40 +0000

    Added the vector package and defined some types

--- src/Hashcode/Types.hs
+++ src/Hashcode/Types.hs
@@ -1,1 +1,17 @@
 module Hashcode.Types where
+
+newtype Megabytes = Megabytes Int
+
+newtype Milliseconds = Milliseconds Int
+
+data ID a = ID { id :: Int }
+
+data Video = Video { size :: Megabytes } deriving (Show, Eq, Ord)
+
+data Endpoint = Endpoint { latencyToDC :: Milliseconds, caches :: Int, cacheLatencies :: HashMap ID Cache Milliseconds}
+
+data Cache = Cache { capacity :: Megabytes} deriving (Show, Eq, Ord)
+
+data Request = Request { video :: ID Video, endpoint :: ID Endpoint, amount :: Int }
+
+data Network = Network {videos :: Vector Video, endpoints :: Vector Endpoint, caches :: Vector Cache, requests :: Vector Request }
